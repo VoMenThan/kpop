df5dcc703907d9aecb27dbdadb5aab2c5a1ecbfc
Men Than <thancn12@gmail.com>
2018-09-25 19:22:01 +0700

    update newsletter

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,6 +102,7 @@ module.exports = function(grunt) {
 		    	'development/index.html',
 		    	'development/category.html',
 		    	'development/detail.html',
+		    	'development/newsletter.html',
 		    	], // Source files
 		    dest: 'production/', // Destination directory
 		    flatten: true,
